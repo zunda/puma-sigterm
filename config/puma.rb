410d3c24eab60386c8ab5d044c62a7a8a927f0c7
zunda <zundan@gmail.com>
2019-11-18 18:15:39 -1000

    Create / and /slow paths

--- config/puma.rb
+++ config/puma.rb
@@ -11,16 +11,35 @@ on_worker_shutdown do
 end
 
 app do |env|
-  puts "[#{$$}] A request is received. Sleeping for 10 seconds"
-  sleep 10
-  body = "Hello, World!\n"
-  puts "[#{$$}] Responding"
-  [
-    200,
-    {
-      'Content-Type' => 'text/plain',
-      'Content-Length' => body.length.to_s
-    },
-    [body]
-  ]
+  case env['REQUEST_URI']
+  when '/'
+    body = "Hello, World!\n"
+    [
+      200,
+      {
+        'Content-Type' => 'text/plain',
+        'Content-Length' => body.length.to_s
+      },
+      [body]
+    ]
+  when '/slow'
+    puts "[#{$$}] A request is received at /slow. Sleeping for 10 seconds"
+    sleep 10
+    body = "Good morning, world!\n"
+    puts "[#{$$}] Responding"
+    [
+      200,
+      {
+        'Content-Type' => 'text/plain',
+        'Content-Length' => body.length.to_s
+      },
+      [body]
+    ]
+  else
+    [
+      404,
+      {},
+      []
+    ]
+  end
 end
