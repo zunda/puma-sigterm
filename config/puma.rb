305ff59540923d5ad2d0156489882d0e9b3196bd
zunda <zundan@gmail.com>
2019-11-18 18:00:31 -1000

    Make Heroku-friendly

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,11 @@
+workers ENV.fetch('WEB_CONCURRENCY') { 1 }
+threads ENV.fetch('MIN_THREADS') { 1 }.to_i, ENV.fetch('MAX_THREADS') { 1 }.to_i
+if ENV['SOCKET']
+  bind "unix://#{ENV['SOCKET']}"
+else
+  bind "tcp://#{ENV.fetch('BIND', '0.0.0.0')}:#{ENV.fetch('PORT', 3000)}"
+end
+
 on_worker_shutdown do
     puts "[#{$$}] Shutting down"
 end
